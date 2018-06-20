4e0c969b6d09e87f41d6aa408a9a039274055953
Trevor Russell <truss.42@gmail.com>
2018-06-20 21:17:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
   
   def valid?
-    if self.sender.valid? && self.receiver.valid?
+    if self.sender.valid? && self.receiver.valid? && self.sender.balance > self.amount
       true
     else
       false
