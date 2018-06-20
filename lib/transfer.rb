cff09d8b87eed10f27b4b5b844218cbd845df3a9
Trevor Russell <truss.42@gmail.com>
2018-06-20 21:07:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,16 +1,16 @@
 class Transfer
   
-  attr_accessor :sender, :reciever, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status
   
-  def initialize(sender, reciever, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
-    @reciever = reciever
+    @reciever = receiver
     @status = 'pending'
     @amount = amount
   end
   
   def valid?
-    if self.sender.valid? && self.reciever.valid?
+    if self.sender.valid? && self.receiver.valid?
       true
     else
       false
