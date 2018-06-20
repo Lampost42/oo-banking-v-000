4e855537198508d8cc698d15d3c29e8276bb9d75
Trevor Russell <truss.42@gmail.com>
2018-06-20 20:56:54 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,12 @@
 class BankAccount
   
-  def initialize
+  attr_reader :name
+  attr_accessor :balance, :status
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
   end
   
 end
